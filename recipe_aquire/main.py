6c07c391e2c860ad3152cc19374a4c84f6e995d5
heildampf <heildampf@gmail.com>
2022-11-12 00:26:14 +0100

    Update main.py

--- recipe_aquire/main.py
+++ recipe_aquire/main.py
@@ -3,12 +3,13 @@ from datetime import date
 
 if __name__ == '__main__':
     categories = ChefKochAPI.get_categories()
-
+    print (categories)
     # recipe_amount = 200
     # recipes = []
     #category = categories[0]
     for category in categories[0:]:
-        if category.title == "Süßspeisen":
+        print (category)
+        if category.title == "Hauptspeise":
             category_recipes = ChefKochAPI.parse_recipes(category)
             DataParser.write_recipes_to_json(str(date.today()) + "-category-" + category.title.replace(" ", "-").replace("/", "-"), category_recipes)
             """ for category_recipe in category_recipes:
